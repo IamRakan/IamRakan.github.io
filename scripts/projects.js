e2c87455e0046a977278989e649933c881b70269
IamRakan <rakanaljohar@outlook.sa>
2022-09-11 14:47:23 +0300

    first commit

---
+++ scripts/projects.js
@@ -0,0 +1,17 @@
+let PROJECTS = [
+    {
+        img: "project1.png",
+        title: "Graduation Project",
+        description: "An efficient and inexpensive to check students' Covid-19 status by using NFC technology."
+    },
+    {
+        img: "project2.png",
+        title: "E-pharmacy",
+        description: "Me and my colleague developed a simple pharmacy application using Android Studio."
+    },
+    {
+        img: "project3.png",
+        title: "Car Rental website",
+        description: "During my internship me and my colleagus made a car rental website using asp.NET."
+    }
+];
